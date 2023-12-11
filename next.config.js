864aa7772d3d99e4f2b999d8abf8ee243272f8c3
UN <59855096+rattamnoon@users.noreply.github.com>
2023-12-11 09:59:40 +0700

    update
    Signed-off-by: UN <59855096+rattamnoon@users.noreply.github.com>

--- next.config.js
+++ next.config.js
@@ -1,8 +1,7 @@
-const million = require('million/compiler');
+const million = require("million/compiler");
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  reactStrictMode: true,
+};
 
-module.exports = million.next(
-  nextConfig
-, { auto: { rsc: true } }
-)
+module.exports = million.next(nextConfig, { auto: { rsc: true } });
